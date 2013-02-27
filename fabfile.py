4ea44e60c7cbbd58ea01049453a58a5a5d188d33
Barry Morrison <b@rrymorrison.com>
2013-02-26 18:44:47 -0800

    because docstrings are good

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,9 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def new_post(title, slug=None):
+    """
+    Creates a new blog post
+    """
     if slug is None:
         slug = slugify.slugify(title)
         header = len(slug) * '#'
@@ -25,18 +28,31 @@ def new_post(title, slug=None):
 
 
 def make_html():
+    """
+    Generates html as defined in MakeFile
+    """
     local("/usr/bin/make html", shell='/bin/bash')
 
 
 def preview():
+    """
+    Generates HTML and spaws firefox to preview in browser
+    """
+    make_html()
     local('firefox public/index.html&')
 
 
 def publish():
+    """
+    Leverages s3cmd to sync public/ to s3 bucket
+    """
     local('/usr/bin/s3cmd sync public/ s3://www.barrymorrison.com/')
 
 
 def render(template, destination, **kwargs):
+    """
+    Function to render new blog post
+    """
     jenv = Environment(loader=FileSystemLoader(['.', ]))
     params = dict(**kwargs)
     text = jenv.get_template(template).render(params)
